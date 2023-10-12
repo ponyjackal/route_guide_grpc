9e9e45421228d8f37173d90d8c156fe06812705d
ponyjackal <pony1125@outlook.com>
2023-10-11 18:45:21 -0700

    feat: add client

--- server/server.go
+++ server/server.go
@@ -191,19 +191,19 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	var opts []grpc.ServerOption
-	if *tls {
-		// if *certFile == "" {
-		// 	*certFile = data.Path("x509/server_cert.pem")
-		// }
-		// if *keyFile == "" {
-		// 	*keyFile = data.Path("x509/server_key.pem")
-		// }
-		// creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
-		// if err != nil {
-		// 	log.Fatalf("Failed to generate credentials: %v", err)
-		// }
-		// opts = []grpc.ServerOption{grpc.Creds(creds)}
-	}
+	// if *tls {
+	// if *certFile == "" {
+	// 	*certFile = data.Path("x509/server_cert.pem")
+	// }
+	// if *keyFile == "" {
+	// 	*keyFile = data.Path("x509/server_key.pem")
+	// }
+	// creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+	// if err != nil {
+	// 	log.Fatalf("Failed to generate credentials: %v", err)
+	// }
+	// opts = []grpc.ServerOption{grpc.Creds(creds)}
+	// }
 
 	grpcServer := grpc.NewServer(opts...)
 	pb.RegisterRouteGuideServer(grpcServer, newServer())
